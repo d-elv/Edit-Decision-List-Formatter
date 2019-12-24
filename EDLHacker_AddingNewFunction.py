c9578c0baf21a881870f4966c927402b1636c479
Daniel Elvey <delvey21@hotmail.co.uk>
2019-12-24 02:24:43 +0000

    It works!!

--- EDLHacker_AddingNewFunction.py
+++ EDLHacker_AddingNewFunction.py
@@ -1,13 +1,12 @@
-import os, csv, re, sys, itertools
-from csv import DictWriter
+import os, csv, re, TechnicallySecondsToTimecode
 from timecode import Timecode
 
 def sixIndexSplitter(splitLine):
     splitLine = re.split(r'\s{2,}', splitter)
     dstIn = splitTimes[2]
     dstOut = splitTimes[3]
-    dstOutSplit = dstOut.split("'")  #Separates timecode from '] at end
-    dstOut = dstOutSplit[0] #isolates timecode into dstOut variable
+    dstOutSplit = dstOut.split("'")  # Separates timecode from '] at end
+    dstOut = dstOutSplit[0]  # isolates timecode into dstOut variable
     dstIn = Timecode('25', dstIn)
     dstOut = Timecode('25', dstOut)
     srcDur = dstOut - dstIn
@@ -17,15 +16,19 @@ def sixIndexSplitter(splitLine):
     splitLine.append(str(srcDur))
     finalFileWriter.writerow(splitLine)
 
+def mergeDict(a, b):
+    a.update(b)
+    return a
+
 
 secondRow = ['Num', 'Reel', 'Tracks', 'Type', 'Src In', 'Src Out', 'Dst In', 'Dst Out', 'Src Dur']
-thirdRow = ['Reel', 'Num', 'Earliest', 'Latest', 'Shortest', 'Longest', 'Total']
+thirdRow = ['Clip', 'Num', 'Earliest', 'Latest', 'Shortest', 'Longest', 'Total']
+print('Drag in the folder containing the file(s) you want to convert. Single files will close the program.')
 directoryToHack = input('Drag in the directory / file you wish to convert and hit enter: ')
-print('Drag in the folder containing the file(s) you want to convert. Dragging in a single file will close the program. ')
 
 acceptedFiletype = ['.edl']
-
 ignoredFiles = []
+
 for (root, paths, file) in os.walk(directoryToHack):
     for f in file:
         filePath = os.path.join(root + os.sep + f)
@@ -100,117 +103,113 @@ for (root, paths, file) in os.walk(directoryToHack):
                     formattedLines.append(splitLine)
                     finalFileWriter.writerow(splitLine)
 
-                #This begins the shortest/longest summary at the bottom of the .edl
-                # finalFile.close()
+                # This begins the shortest/longest summary at the bottom of the .edl
+
                 print('Writing second section to formatted EDL file...')
                 finalList = []
-                for rowItem in thirdRow:
-                    finalList += [str(rowItem)]
                 finalFileWriter.writerow(finalList)
+                numCounter = 0
+                clipCount = []
                 reelCount = ''
                 reel = ''
-                numV = ''
-                earliestSRC = ''
-                latestSRC = ''
-                shortestV = ''
-                longestV = ''
-                totalTC = ''
+                numV = 1
+                blankTC = '00:00:00:00'
+                blankTC = Timecode('25', blankTC)
+                earliestSRC = '00:00:00:00'
+                earliestSRC = Timecode('25', earliestSRC)
+                latestSRC = '00:00:00:00'
+                latestSRC = Timecode('25', latestSRC)
+                shortestV = '00:00:00:00'
+                shortestV = Timecode('25', shortestV)
+                longestV = '00:00:00:00'
+                longestV = Timecode('25', longestV)
+                totalTC = '00:00:00:00'
+                totalTC = Timecode('25', totalTC)
+                srcTCSum = 0
+                reels = {}
                 # Defined variables to be used in the nexted Dict that will track data
-                reels = {reelCount : {'Clip' : reel, 'Num' : numV, 'Earliest' : earliestSRC, 'Latest' : latestSRC,
-                                         'Shortest' : shortestV, 'Longest' : longestV, 'Total' : totalTC}}
                 # Nested Dict that will track data. Keys and values to see what's happening.
+
                 for line in formattedLines:
                     lineSplit = str(line).split()
-                    if lineSplit[1] not in reels[reelCount]['Clip']:
-                        reelNew = lineSplit[1]
-                        updaterDict = {reelNew : {'Clip' : lineSplit[1]}}
-                        reels.update(updaterDict)
+                    #If a clip name is not in reels Nested Dict, adds it to clip Key value, creating new dicts for each.
+                    if lineSplit[1] not in reels.keys():
+                        reels[lineSplit[1]] = {'Clip' : reel, 'Num' : numV, 'Earliest' : earliestSRC, 'Latest' : latestSRC,
+                                                  'Shortest' : shortestV, 'Longest' : longestV, 'Total' : totalTC}
+                        reels[lineSplit[1]]['Earliest'] = lineSplit[4]
+                        reels[lineSplit[1]]['Latest'] = lineSplit[5]
+                        reels[lineSplit[1]]['Shortest'] = lineSplit[8]
+                        reels[lineSplit[1]]['Longest'] = lineSplit[8]
+                        reels[lineSplit[1]]['Total'] = blankTC
 
+                        # srcTCPerLine = lineSplit[8]
+                        # srcTCPerLine = srcTCPerLine[0:11]
+                        # srcTCPerLine = FramesToTimecode.tc_to_fr(srcTCPerLine)
+                        # srcTCSum = srcTCSum + srcTCPerLine
+                    else:
+                        reels[lineSplit[1]]['Num'] += 1
 
+                    # Adds up of all the Source Timecode amounts for each individual clip
 
-                        # reels[reelCount]['Clip'] = lineSplit[1]
+                    earliestSRC = reels[lineSplit[1]].get('Earliest')
+                    earliestSRC = earliestSRC[1:12]
+                    earliestSRCTemp = lineSplit[4] #Isolate the potential earliest Source TC from formattedLines
+                    earliestSRCTemp = earliestSRCTemp[1:12] #Chop out the punctuation to allow it to be read by fr_to_tc
+                    earliestSRCinfr = TechnicallySecondsToTimecode.tc_to_fr(earliestSRC) #turn potential earliest TC into no. of fr.
+                    earliestSRCTempinfr = TechnicallySecondsToTimecode.tc_to_fr(earliestSRCTemp)
 
-                # dictWriter = csv.DictWriter(finalFile, reels)
-                # for key,val in sorted(reels.items()):
-                #     row = {'Clip': key}
-                #     row.update(val)
-                #     print(row)
-                #     dictWriter.writerow(row)
+                    if earliestSRCTempinfr < earliestSRCinfr or earliestSRCTempinfr == earliestSRCinfr:
+                        earliestSRCTemp = TechnicallySecondsToTimecode.fr_to_tc(earliestSRCTempinfr)
 
-                #TODO - Go through formattedLines list, create a summary starting with reel
+                        reels[lineSplit[1]]['Earliest'] = earliestSRCTemp
 
-                #Increment through formattedLines, for every new reel, add a new key to the reels dict.
-                #For every same reel, update the information in its respective key value pairing.
+                    # This section pulls the latest source Timecode that appears and continually updates.
+                    latestSRC = reels[lineSplit[1]].get('Latest')
+                    latestSRC = latestSRC[1:12]
+                    latestSRCTemp = lineSplit[5]  # Isolate the potential latest Source TC from formattedLines
+                    latestSRCTemp = latestSRCTemp[1:12]
+                    latestSRCinfr = TechnicallySecondsToTimecode.tc_to_fr(latestSRC)
+                    latestSRCTempinfr = TechnicallySecondsToTimecode.tc_to_fr(latestSRCTemp)
 
+                    if latestSRCTempinfr > latestSRCinfr or latestSRCTempinfr == latestSRCinfr:
+                        latestSRCTemp = TechnicallySecondsToTimecode.fr_to_tc(latestSRCTempinfr)
 
+                        reels[lineSplit[1]]['Latest'] = latestSRCTemp
 
+                    # This section pulls the Shortest and Longest instances of a clip.
 
+                    shortestSRC = reels[lineSplit[1]].get('Shortest')
+                    shortestSRC = shortestSRC[0:11]
+                    shortestSRCTemp = lineSplit[8]
+                    shortestSRCTemp = shortestSRCTemp[0:11]
+                    shortestSRCinfr = TechnicallySecondsToTimecode.tc_to_fr(shortestSRC)
+                    shortestSRCTempinfr = TechnicallySecondsToTimecode.tc_to_fr(shortestSRCTemp)
 
+                    if shortestSRCTempinfr < shortestSRCinfr or shortestSRCTempinfr == shortestSRCinfr:
+                        shortestSRCTemp = TechnicallySecondsToTimecode.fr_to_tc(shortestSRCTempinfr)
 
+                        reels[lineSplit[1]]['Shortest'] = shortestSRCTemp
 
+                    if shortestSRCTempinfr > shortestSRCinfr or shortestSRCTempinfr == shortestSRCinfr:
+                        shortestSRCTemp = TechnicallySecondsToTimecode.fr_to_tc(shortestSRCTempinfr)
 
+                        reels[lineSplit[1]]['Longest'] = shortestSRCTemp
 
+                    totalTC = reels[lineSplit[1]].get('Total')  # Takes current reel's Total source TC count
+                    totalTCTemp = lineSplit[8]  # Takes current line's source duration.
+                    totalTCTemp = totalTCTemp[0:11]
+                    totalTCinfr = TechnicallySecondsToTimecode.tc_to_fr(totalTC) # Turns current total TC into no. of frames
+                    totalTCTempinfr = TechnicallySecondsToTimecode.tc_to_fr(totalTCTemp) # Turns current line's TC into no. of frames
 
+                    totalTCinfr = totalTCinfr + totalTCTempinfr
+                    totalTCinfr = TechnicallySecondsToTimecode.fr_to_tc(totalTCinfr)
+                    reels[lineSplit[1]]['Total'] = totalTCinfr
+
+                # Write the nestedDict created in the previous for loop to the .csv file.
+
+                w = csv.DictWriter(finalFile, thirdRow)
+                w.writeheader()
+                for k in sorted(reels):
+                    w.writerow({field: reels[k].get(field) or k for field in thirdRow})
                 finalFile.close()
-
             else: print(name + ext + ' ignored.')
-
-                    # splitter = str(i)
-                    # splitLine = splitter.split()
-                    # if len(splitLine) <= 6:
-                    #     #sixIndexSplitter(splitLine)
-                    #     continue
-                    # splitTimes = str(splitLine[4]).split()
-                    # num = splitLine[0]
-                    # #num = num[2:]
-                    # dstIn = splitLine[6]
-                    # dstOut = splitLine[7]
-                    #
-                    # if ('"]') in dstOut:
-                    #     dstOutSplit = dstOut.split('"')
-                    #     dstOut = dstOutSplit[0]
-                    #     dstIn = Timecode('25', dstIn)
-                    #     dstOut = Timecode('25', dstOut)
-                    #     srcDur = dstOut - dstIn
-                    #     del splitLine[0]
-                    #     splitLine.insert(0, num)
-                    #     del splitLine[-1]
-                    #     splitLine.append(str(srcDur))
-                    #     #finalFileWriter.writerow(splitLine)
-                    #     continue
-                    #
-                    # dstOutSplit = dstOut.split("'")  #Separates timecode from '] at end
-                    # dstOut = dstOutSplit[0] #isolates timecode into dstOut variable
-                    # dstIn = Timecode('25', dstIn)
-                    # dstOut = Timecode('25', dstOut)
-                    # srcDur = dstOut - dstIn
-                    # del splitLine[0]
-                    # splitLine.insert(0, num)
-                    # splitLine.append(srcDur)
-                    # del splitLine[2:4]      #From this point the new section begins formatting.
-                    # result2 = []            #Deletes Tracks and Source
-                    #
-                    # num2 = splitLine[0]
-                    # reel2 = splitLine[1]
-                    # earliest2 = splitLine[2]
-                    # earliest2TC = Timecode('25', earliest2)
-                    # latest2 = splitLine[3]
-                    # latest2TC = Timecode('25', latest2)
-                    # shortest2 = str(splitLine[6])
-                    # shortest2TC = Timecode('25', shortest2)
-                    # longest2 = str(splitLine[6])
-                    # longest2TC = Timecode('25', longest2)
-                    #
-                    # lineForDict = [num2, earliest2TC, latest2TC, shortest2TC, longest2TC]
-                    #
-                    # dictionary = {reel2: [lineForDict]}
-                    #
-                    # if reel2 not in dictionary:
-                    #     if num2 not in lineForDict:
-                    #
-                    #
-                    #
-                    #
-                    #
-                    #
-                    # print(splitLine)
